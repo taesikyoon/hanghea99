436a494a8761c9ad0812ed01b540c38bc30c5e2b
Taesik yoon <taesikyoon97@naver.com>
2022-07-07 01:44:13 +0900

    0706

---
+++ Javascript 개인공부/005.js
@@ -0,0 +1,10 @@
+let name = "peter";
+let number = 200;
+let isTrue = true;
+let nothing = null;
+let empty = undefined;
+let list = [];
+let ref = {};
+let fucn = function(){};
+
+a = 5; // 별도의 키워드 없이 변수를 할당하는 방법을 자바스크립트에서는 암시적 선언이라 한다. ( 권장하지 않는 편 )
