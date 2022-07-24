66888deb76a3e7546a6b01e76cdb7904e7b240f0
Taesik yoon <taesikyoon97@naver.com>
2022-07-24 22:44:06 +0900

    문자열 내 마음대로 정리하기

---
+++ second-week/19.js
@@ -0,0 +1,14 @@
+function solution(strings, n) {
+    var answer = [];
+    for (var i = 0; i < strings.length; i++) {
+        var chu = strings[i][n];
+        strings[i] = chu + strings[i];
+    }
+    strings.sort();
+    for (var j = 0; j < strings.length; j++) {
+        strings[j] = strings[j].replace(strings[j][0],"");
+        answer.push(strings[j])
+    }
+
+    return answer;
+}
